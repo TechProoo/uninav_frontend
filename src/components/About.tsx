07828676ac3a20dcae6bdeb0ebd3730ae81ba1a1
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2025-04-05 00:08:13 +0100

    Hero Created

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mt-10 about_bg">
+      <div className="w-10/12 m-auto">
+        <div className="grid grid-cols-12 items-center">
+          <div className="md:col-span-6">
+            <div className="border_c"></div>
+          </div>
+          <div className="md:col-span-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
